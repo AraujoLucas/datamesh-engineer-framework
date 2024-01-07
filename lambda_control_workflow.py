3f406cf2b7822b41a73c2f1d2a85050f11049b64
Araujo Engineer <lucasaraujo271094@gmail.com>
2024-01-07 18:21:56 -0300

    refactory v.2 lambda_control_workflow.py

--- lambda_control_workflow.py
+++ lambda_control_workflow.py
@@ -6,84 +6,86 @@
   }
 }
 '''
-
 import json
 import boto3
 
-dynamodb_client = boto3.client('dynamodb')
-
-def extract_params(record_kafka):        
+def extract_params(record_kafka):
     table_name = record_kafka['detail']['tableName']
     changed_partitions = record_kafka['detail']['changedPartitions'][0]
-
-    print(f'> table name: {table_name}')
-    print(f'> partition value: {changed_partitions}')
-
     return table_name, changed_partitions
 
-def get_item_ddb(dynamodb_client,ddb_control,changed_partitions):
-    response = dynamodb_client.get_item(
-            TableName= ddb_control,
+def get_item_ddb(dynamodb_client, ddb_control, changed_partitions):
+    try:
+        response = dynamodb_client.get_item(
+            TableName=ddb_control,
             Key={"PartitionKey": {"S": changed_partitions}}
         )
-    item_exists = 'Item' in response
-    print(f'> Item is {item_exists}')
-    return item_exists
-    
-def insert_ddb(dynamodb_client,ddb_control,item_exists,changed_partitions,table_name):
-    
-    if item_exists:
-        # realizar update no item existente adicionando nova tb e pt disponivel para ETL
-        print(f'> update the existing key {changed_partitions} adding new table {table_name} and partition available')
-        
-        updated_second_table_name = f"{table_name}"
-        updated_status_trigger = 'start ETL'
-        update_expression = 'SET #partition_status = :partition_status, #table_2 = :updated_table, #trigger = :trigger'
-        expression_attribute_names = {'#partition_status': 'table_2_partitionStatus', '#table_2': 'table_2', '#trigger': 'trigger'}
-        expression_attribute_values = {
-                    ':partition_status': {'S': 'disponivel'},
-                    ':updated_table': {'S': updated_second_table_name},
-                    ':trigger': {'S': updated_status_trigger}
-                }
-        
+        return 'Item' in response
+    except Exception as e:
+        print(f'Error in get_item_ddb: {e}')
+        return False
+
+def update_item_ddb(dynamodb_client, ddb_control, changed_partitions, table_name):
+    updated_second_table_name = f"{table_name}"
+    updated_status_trigger = 'start ETL'
+    update_expression = 'SET #partition_status = :partition_status, #table_2 = :updated_table, #trigger = :trigger'
+    expression_attribute_names = {'#partition_status': 'table_2_partitionStatus', '#table_2': 'table_2', '#trigger': 'trigger'}
+    expression_attribute_values = {
+        ':partition_status': {'S': 'disponivel'},
+        ':updated_table': {'S': updated_second_table_name},
+        ':trigger': {'S': updated_status_trigger}
+    }
+
+    try:
         update_item = dynamodb_client.update_item(
-                TableName= ddb_control,
-                Key={"PartitionKey": {"S": changed_partitions}},
-                UpdateExpression=update_expression,
-                ExpressionAttributeNames=expression_attribute_names,
-                ExpressionAttributeValues=expression_attribute_values
-            )
-        status = update_item['ResponseMetadata']['HTTPStatusCode']
-        print(f'> status code for update_item in ddb: {status} ')
-        
-    else:
-        print(f'> the item does not exist in ddb, executing the insert task: {changed_partitions}')
-        dynamodb_payload = {
-            "TableName": "ddb_control_ingestion",
-            "Item": {
-                "PartitionKey": {"S": changed_partitions},
-                "table_1": {"S": table_name},
-                "table_1_partitionStatus": {"S": "disponivel"}
-            }
+            TableName=ddb_control,
+            Key={"PartitionKey": {"S": changed_partitions}},
+            UpdateExpression=update_expression,
+            ExpressionAttributeNames=expression_attribute_names,
+            ExpressionAttributeValues=expression_attribute_values
+        )
+        return update_item['ResponseMetadata']['HTTPStatusCode']
+    except Exception as e:
+        print(f'Error in update_item_ddb: {e}')
+        return None
+
+def insert_item_ddb(dynamodb_client, ddb_control, changed_partitions, table_name):
+    dynamodb_payload = {
+        "TableName": ddb_control,
+        "Item": {
+            "PartitionKey": {"S": changed_partitions},
+            "table_1": {"S": table_name},
+            "table_1_partitionStatus": {"S": "disponivel"}
         }
+    }
+
+    try:
         response_put_item = dynamodb_client.put_item(**dynamodb_payload)
-        status = response_put_item['ResponseMetadata']['HTTPStatusCode']
-        print(f'> status code for put_item in ddb: {status} ')
-        
-    return 0
+        return response_put_item['ResponseMetadata']['HTTPStatusCode']
+    except Exception as e:
+        print(f'Error in insert_item_ddb: {e}')
+        return None
 
 def lambda_handler(event, context):
-    record_kafka=event
-    ddb_control = 'name_ddb_control_workflow'
-    
-    print(f'::: running task extract params for job :::')
-    table_name, changed_partitions = extract_params(record_kafka)
-    
-    print(f'::: running task if item already exists :::')
-    item_exists = get_item_ddb(dynamodb_client,ddb_control,changed_partitions)
-    
-    print(f'::: running task insert in ddb :::')
-    insert_ddb(dynamodb_client,ddb_control,item_exists,changed_partitions,table_name)
+    dynamodb_client = boto3.client('dynamodb')
+    record_kafka = event
+    ddb_control = 'ddb_control_ingestion'
 
-    return print('::: Processing executed successfully! :::')
+    print('::: Running task extract params for job :::')
+    table_name, changed_partitions = extract_params(record_kafka)
+
+    print('::: Running task if item already exists :::')
+    item_exists = get_item_ddb(dynamodb_client, ddb_control, changed_partitions)
+
+    print('::: Running task insert/update in DynamoDB :::')
+    if item_exists:
+        status = update_item_ddb(dynamodb_client, ddb_control, changed_partitions, table_name)
+    else:
+        status = insert_item_ddb(dynamodb_client, ddb_control, changed_partitions, table_name)
+
+    if status is not None:
+        print(f'::: Processing executed successfully Status Code: {status}')
+    else:
+        print('::: Processing failed!')
+
     
